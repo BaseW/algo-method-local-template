a8a75d7ae3aef58750e20da789735f9a8cb0b263
wada <wada@avita.co.jp>
2022-06-19 22:33:16 +0900

    answer for question 61

---
+++ questions/61/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    // read buffer from stdin
+    let mut buffer = String::new();
+    std::io::stdin().read_line(&mut buffer).unwrap();
+    // parse buffer as i64
+    let n: i64 = buffer.trim().parse().unwrap();
+
+    // read n numbers splitted by space from stdin
+    let mut buffer = String::new();
+    std::io::stdin().read_line(&mut buffer).unwrap();
+    // parse buffer as Vec<i64>
+    let numbers: Vec<i64> = buffer
+        .split_whitespace()
+        .map(|x| x.parse().unwrap())
+        .collect();
+
+    // print minimum of numbers
+    println!("{}", numbers.iter().min().unwrap());
+}
