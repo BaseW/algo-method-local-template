6a0870c8e428d10f6689fd92da61551051ad7273
wada <wada@avita.co.jp>
2022-06-28 20:20:53 +0900

    answer for question 231

---
+++ questions/231/main.rs
@@ -0,0 +1,41 @@
+fn main() {
+    // read usize n and m splitted by space from stdin
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    let mut iter = input.split_whitespace();
+    let n: usize = iter.next().unwrap().parse().unwrap();
+    let m: usize = iter.next().unwrap().parse().unwrap();
+
+    // read Vec<usize> numbers_a splitted by space from stdin
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    let numbers_a: Vec<usize> = input
+        .split_whitespace()
+        .map(|x| x.parse().unwrap())
+        .collect();
+
+    // read Vec<usize> numbers_b splitted by space from stdin
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    let numbers_b: Vec<usize> = input
+        .split_whitespace()
+        .map(|x| x.parse().unwrap())
+        .collect();
+
+    // initialize result by 0
+    let mut result = 0;
+    // i is 0 <= n - 1, j is 0 <= m - 1
+    // numbers_a.len() is n
+    // numbers_b.len() is m
+    // check count for numbers_a[i] > numbers_b[j]
+    for i in 0..numbers_a.len() {
+        for j in 0..numbers_b.len() {
+            if numbers_a[i] > numbers_b[j] {
+                result += 1;
+            }
+        }
+    }
+
+    // print result
+    println!("{}", result);
+}
